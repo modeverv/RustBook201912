ee77e3a9056f89ee9055e8bbfcb05cb5d51930b7
modeverv <modeverv@gmail.com>
2019-12-19 00:10:32 +0900

    2019-12-19 00:10:32

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,26 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    let args:Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
     //println!("{:?}",args)
-    
+    let config = parse_config(&args);
+    println!("Search for '{}' Infile '{}'", config.query, config.filename);
+
+    let mut f = File::open(config.filename).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    println!("With text:\n{}",contents);
+
+}
+struct Config {
+    query: String,
+    filename: String,
+}
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config { query, filename }
 }
