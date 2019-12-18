5e233cee3a10ec0d7410e54ed8b141034db5989e
modeverv <modeverv@gmail.com>
2019-12-19 00:18:43 +0900

    2019-12-19 00:18:43

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,33 +1,40 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     //println!("{:?}",args)
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     println!("Search for '{}' Infile '{}'", config.query, config.filename);
 
+    run(config);
+}
+fn run(config: Config) {
     let mut f = File::open(config.filename).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
-    println!("With text:\n{}",contents);
-
+    println!("With text:\n{}", contents);
 }
+
 struct Config {
     query: String,
     filename: String,
 }
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            // 引数の数が足りません
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
+
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
