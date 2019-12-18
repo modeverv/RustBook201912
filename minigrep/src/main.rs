e763eedc9a8e9d0d9b367ecdf1b6c09815b6b6d4
modeverv <modeverv@gmail.com>
2019-12-19 00:38:13 +0900

    2019-12-19 00:38:13

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,13 +5,10 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //println!("{:?}",args)
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("problem parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("Search for '{}' Infile '{}'", config.query, config.filename);
-
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
